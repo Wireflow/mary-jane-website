b988d0dd9fb6d3df4a5c39dbb24487337cbc5a4c
kuroafa <kuroafa21@gmail.com>
2024-01-04 21:56:14 -0500

    refactor: news banner

--- src/components/layout/NewsBanner.tsx
+++ src/components/layout/NewsBanner.tsx
@@ -7,7 +7,7 @@ type NewsBannerProps = {
 
 const NewsBanner = (props: NewsBannerProps) => {
   return (
-    <div className="bg-theme-yellow py-3 text-center">
+    <div className="bg-theme-yellow py-3 text-center flex justify-center sm:flex-row flex-col">
       {props.text} <span className="font-bold">{props.newsText}</span>
     </div>
   );

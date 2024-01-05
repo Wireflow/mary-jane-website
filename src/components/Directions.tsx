189f29e6542a39c5c1fb120c3de34d47f3d89e3b
kuroafa <kuroafa21@gmail.com>
2024-01-04 20:21:25 -0500

    refactor: changed directions container back to black

--- src/components/Directions.tsx
+++ src/components/Directions.tsx
@@ -6,10 +6,10 @@ type Props = {};
 
 const Directions = (props: Props) => {
   return (
-    <Section className="bg-white" size="sm">
+    <Section className="bg-black" size="sm">
       <div className="flex flex-col items-center gap-6">
-        <h1 className="lg:text-7xl md:text-5xl sm:text-4xl text-2xl text-black font-bold">
-          Where you can <span className="text-theme-purple">find</span> us
+        <h1 className="lg:text-6xl md:text-5xl sm:text-4xl text-2xl text-white font-semibold">
+          Where you can <span className="text-theme-yellow">find</span> us
         </h1>
         <Button className="sm:px-[50px] px-[30px] py-5 text-[12px] bg-theme-yellow text-black font-bold">
           Get Directions

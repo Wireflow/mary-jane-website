c51cba1b2c9ce211160ebea28c91bc463befad47
kuroafa <kuroafa21@gmail.com>
2024-01-07 23:13:36 -0500

    deploy fix # 7

--- next.config.js
+++ next.config.js
@@ -6,15 +6,6 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        fs: false,
-      };
-    }
-
-    return config;
-  },
 };
 
 module.exports = nextConfig;

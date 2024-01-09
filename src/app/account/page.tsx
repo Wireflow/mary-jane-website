4e62ecc19b397bea1249ba7a0c505f0190042723
kuroafa <kuroafa21@gmail.com>
2024-01-08 22:42:35 -0500

    ...

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -2,7 +2,7 @@ import AccountPage from "@/components/pages/AccountPage";
 import { authOptions } from "@/services/next-auth";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
-import { db } from "../../services/db";
+import { db } from "../../../prisma";
 
 type Props = {};
 

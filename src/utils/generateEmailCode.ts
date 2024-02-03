90700a3ea87cb14045c9d3957b18fe19e1904e56
Nader Abdulrub <nabdulrub16@gmail.com>
2024-02-02 21:27:30 -0500

    refactor: new auth page.

--- src/utils/generateEmailCode.ts
+++ src/utils/generateEmailCode.ts
@@ -1,11 +1,13 @@
 export default function generateVerificationCode() {
   const digits = "0123456789";
   let code = "";
-  while (code.length !== 6) {
+
+  while (code.length < 7) {
     for (let i = 0; i < 6; i++) {
       code += digits[Math.floor(Math.random() * 10)];
     }
   }
+
   const expiration = new Date();
   expiration.setHours(expiration.getHours() + 1);
   return { code: Number(code), expiration };
